486d089c37eca3bad9738b1245e618b079cc70d4
jangr <jan.gruca1@gmail.com>
2023-02-07 16:22:24 +0100

    Finish

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -7,32 +6,54 @@ public class Main {
         Car S3 = new Car(120000.0, "Audi", 30000.0, "Czarny", "Premium",true,true,true,true,true);
         Customer Maniek = new Customer(200000.0,"Mercedes","Audi",false,false);
         Car VW = new Car(19000.0, "Volkswagen", 201294.0, "Niebieski","Budget", true, false, true, true, true);
+        Customer Wiesiek = new Customer(100000.0,"BMW","Audi",false, false);
 
-//        System.out.println(S3.Wartosc);
-//        System.out.println(S3.Przebieg);
-//        System.out.println(S3.Marka);
+        Scanner scanner = new Scanner(System.in);
+        Owner me = new Owner();
+        System.out.println("Witaj w symulatorze komisu samochodowego");
+        System.out.println("Masz do wyboru:");
+        System.out.println("1. Przeglądaj bazę samochodów do kupienia");
+        System.out.println("2. Kup samochód");
+        System.out.println("3. Przeglądaj bazę posiadanych samochodów");
+        System.out.println("4. Napraw samochód");
+        System.out.println("5. Przeglądaj bazę potencjalnych klientów");
+        System.out.println("6. Sprzedaj samochód klientowi");
+        System.out.println("7. Sprawdź stan konta");
+        int choice = scanner.nextInt();
+        while (choice < 1 || choice > 7){
+            System.out.print("Wybierz prawidłową opcję 1-7: ");
+            choice = scanner.nextInt();
+        }
+        if (choice == 1){
+            me.generateBase();
+        }
+        if (choice == 2) {
+            System.out.println("Wpisz, który samochód chcesz kupić:");
+            String answer = scanner.nextLine();
+            me.buy(S3); //me.buy(answer)
 
-//        Scanner Scanner = new Scanner(System.in);
-//
-//        Double choice = Scanner.nextDouble();
-//
-//        Switch(choice){
-//            case 1:
-//                System.out.println("Opcja nr 1");
-//                break;
+        }
+        if (choice == 3){
+            System.out.println(me.garage);
+        }
+        if (choice == 4){
+            System.out.println("Wybierz, u którego mechanika chcesz naprawić samochód:\n1. Janusz\n2. Mariusz\n3. Adrian");
+            String choice1;
+            choice1 = scanner.nextLine();
 
-        Owner marek = new Owner();
-        System.out.println(marek.wallet);
-        marek.buy(S3,Maniek);
-        System.out.println(marek.wallet);
-
-        marek.sell(S3,Maniek);
-        System.out.println(marek.wallet);
-        System.out.println(VW.Zawieszenie);
-        marek.repair(VW,"Janusz");
-        System.out.println(VW.Zawieszenie);
-        System.out.println(marek.wallet);
-
+            me.repair(S3,choice1);
+            System.out.println("Samochód naprawiony");
+        }
+        if(choice == 5){
+            System.out.println(me.customers);
+        }
+        if(choice == 6){
+            me.sell(S3,Maniek);
+            System.out.println("Samochód sprzedany");
+        }
+        if(choice == 7){
+            System.out.println("Stan konta: " + me.wallet);
+        }
         }
     }
 
--- src/Owner.java
+++ src/Owner.java
@@ -6,8 +6,9 @@ public class Owner {
     private static final Double DEFAULT_WALLET_STATUS = 60000.0;
     Double wallet;
     Double tax = 0.02;
-    ArrayList<Car> garage;
-    ArrayList<Customer> customers;
+    static ArrayList<Car> garage;
+    static ArrayList<Customer> customers;
+    Integer moves = 0;
 
 
     public Owner(){
@@ -16,12 +17,12 @@ public class Owner {
         customers = new ArrayList<>();
     }
 
-    public void buy(Car car, Customer customer){
+    public void buy(Car car){
         this.wallet -= car.Wartosc;
         this.wallet -= tax * car.Wartosc;
         garage.add(car);
-        customers.add(customer);
-        customers.add(customer);
+        moves += 1;
+
     }
 
     public void sell(Car car, Customer customer){
@@ -30,17 +31,9 @@ public class Owner {
         garage.remove(car);
         customers.add(customer);
         customers.add(customer);
+        moves += 1;
     }
 
-//    public Double repairCost(Car car, String mechanik, Double repairCost){
-//        return
-//                switch (mechanik){
-//                    case "Janusz":
-//                        repairCost = 0.1* car.Wartosc;
-//                        break;
-//
-//                }
-//    }
 
     private Integer repairCost(Car car, String mechanik){
         int cost = 0;
@@ -213,6 +206,27 @@ public class Owner {
         switch (mechanik) {
             case "Janusz" -> {
                 wallet -= repairCost(car, mechanik);
+                if(!car.Hamulce){
+                    car.Wartosc*=1.1;
+                    car.Hamulce = true;
+                }
+                if(!car.Silnik){
+                    car.Wartosc*=2;
+                    car.Silnik = true;
+                }
+                if(!car.Karoseria){
+                    car.Wartosc*=1.5;
+                    car.Karoseria = true;
+                }
+                if(!car.Zawieszenie){
+                    car.Wartosc*=1.2;
+                    car.Zawieszenie = true;
+                }
+                if(!car.SkrzyniaBiegow){
+                    car.Wartosc*=1.5;
+                    car.SkrzyniaBiegow = true;
+                }
+                moves += 1;
 
             }
             case "Marian" -> {
@@ -223,6 +237,27 @@ public class Owner {
                     wallet -= repairCost(car, "Janusz");
                 } else {
                     wallet -= repairCost(car, mechanik);
+                    if (!car.Hamulce) {
+                        car.Wartosc *= 1.1;
+                        car.Hamulce = true;
+                    }
+                    if (!car.Silnik) {
+                        car.Wartosc *= 2;
+                        car.Silnik = true;
+                    }
+                    if (!car.Karoseria) {
+                        car.Wartosc *= 1.5;
+                        car.Karoseria = true;
+                    }
+                    if (!car.Zawieszenie) {
+                        car.Wartosc *= 1.2;
+                        car.Zawieszenie = true;
+                    }
+                    if (!car.SkrzyniaBiegow) {
+                        car.Wartosc *= 1.5;
+                        car.SkrzyniaBiegow = true;
+                    }
+                    moves += 1;
                 }
             }
             case "Adrian" -> {
@@ -233,8 +268,86 @@ public class Owner {
                     wallet -= repairCost(car, "Janusz");
                 } else {
                     wallet -= repairCost(car, mechanik);
+                    if(!car.Hamulce){
+                        car.Wartosc*=1.1;
+                        car.Hamulce = true;
+                    }
+                    if(!car.Silnik){
+                        car.Wartosc*=2;
+                        car.Silnik = true;
+                    }
+                    if(!car.Karoseria){
+                        car.Wartosc*=1.5;
+                        car.Karoseria = true;
+                    }
+                    if(!car.Zawieszenie){
+                        car.Wartosc*=1.2;
+                        car.Zawieszenie = true;
+                    }
+                    if(!car.SkrzyniaBiegow){
+                        car.Wartosc*=1.5;
+                        car.SkrzyniaBiegow = true;
+                    }
+                    moves += 1;
                 }
             }
         }
+    }
+
+    static ArrayList<Car> bazaSamochodow = new ArrayList<>();
+    Car car1 = new Car(250000.0, "Audi", 943.0, "Czarny","Premium", true, true, true, true, true);
+    Car car2 = new Car(200000.0, "BMW", 532.0, "Srebrny","Premium", true, true, true, true, true);
+    Car car3 = new Car(150000.0, "Mercedes", 9821.0, "Biały","Premium", true, true, true, true, true);
+    Car car4 = new Car(100000.0, "Volkswagen", 2094.0, "Czarny","Standard", true, true, true, true, true);
+    Car car5 = new Car(50000.0, "Opel", 82384.0, "Czarny","Standard", false, true, true, true, true);
+    Car car6 = new Car(25000.0, "Toyota", 121947.0, "Biały","Budget", true, false, true, true, true);
+    Car car7 = new Car(10000.0, "BMW", 419284.0, "Zielony","Budget", true, true, false, true, true);
+    Car car8 = new Car(5000.0, "Skoda", 306284.0, "Niebieski","Budget", true, true, false, false, true);
+    Car car9 = new Car(3250000.0, "Pagani", 21.0, "Czerwony","Premium", true, true, true, true, true);
+    Car car10 = new Car(80000.0, "Skoda", 3042.0, "Biały","Standard", true, true, true, true, true);
+    Car car11 = new Car(130000.0, "Hyundai", 376.0, "Szary","Standard", true, true, true, true, true);
+    Car car12 = new Car(170000.0, "Ford", 11947.0, "Niebieski","Premium", true, true, true, true, true);
+    Car car13 = new Car(30000.0, "Nissan", 110284.0, "Czarny","Standard", true, true, true, true, false);
+    Car car14 = new Car(67000.0, "Peugeot", 120206.0, "Fioletowy","Premium", true, true, true, true, true);
+    Car car15 = new Car(32000.0, "Citroen", 290927.0, "Szary","Budget", true, true, true, false, true);
+    Car car16 = new Car(90000.0, "Skoda", 257384.0, "Srebrny","Standard", true, true, true, true, true);
+    Car car17 = new Car(15000.0, "Toyota", 321935.0, "Biały","Budget", true, true, true, false, true);
+    Car car18 = new Car(8000.0, "Opel", 330195.0, "Szary","Budget", true, true, false, true, true);
+    Car car19 = new Car(19000.0, "Volkswagen", 201294.0, "Niebieski","Budget", true, false, true, true, true);
+    Car car20 = new Car(37000.0, "Mercedes", 121294.0, "Srebrny","Standard", false, true, true, true, true);
+    Car car21 = new Car(46000.0, "BMW", 61294.0, "Czarny","Standard", true, true, true, true, false);
+    Car car22 = new Car(400000.0, "Audi", 3.0, "Zielony","Premium", true, true, true, true, true);
+
+
+    public void generateBase() {
+        bazaSamochodow.add(car1);
+        bazaSamochodow.add(car2);
+        bazaSamochodow.add(car3);
+        bazaSamochodow.add(car4);
+        bazaSamochodow.add(car5);
+        bazaSamochodow.add(car6);
+        bazaSamochodow.add(car7);
+        bazaSamochodow.add(car8);
+        bazaSamochodow.add(car9);
+        bazaSamochodow.add(car10);
+        bazaSamochodow.add(car11);
+        bazaSamochodow.add(car12);
+        bazaSamochodow.add(car13);
+        bazaSamochodow.add(car14);
+        bazaSamochodow.add(car15);
+        bazaSamochodow.add(car16);
+        bazaSamochodow.add(car17);
+        bazaSamochodow.add(car18);
+        bazaSamochodow.add(car19);
+        bazaSamochodow.add(car20);
+        bazaSamochodow.add(car21);
+        bazaSamochodow.add(car22);
+        for(int i = 0; i < 10; i++) {
+            Random random = new Random();
+            int randomIndex = random.nextInt(bazaSamochodow.size());
+            Car selectedCar = bazaSamochodow.get(randomIndex);
+            System.out.println(selectedCar);
+        }
+
     }
 }
